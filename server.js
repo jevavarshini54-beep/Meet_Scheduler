9b7430ae6826839004e8dec56a13ce1039d63fa0
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-22 11:55:04 +0530

    Initial setup: Express server with Socket.IO running on port 5000

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const http = require('http');
+const {Server} = require('socket.io');
+dotenv.config();
+
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+const server = http.createServer(app);
+const io = new Server(server, {cors: {origin : '*'}});
+
+app.get('/', (req, res) => {
+  res.send('Meeting Scheduler backend is running!');
+});
+
+io.on('connection', (socket) => {
+  console.log('A user connected:', socket.id);
+  socket.on('disconnect', () => {
+    console.log('User disconnected:', socket.id);
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
