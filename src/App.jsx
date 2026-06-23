50cbe0b7640cc038db435d45f57735db44b1d7b2
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-23 14:11:06 +0530

    Used POST and DELETE methods to create and delete a meeting

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import Login from './pages/Login';
 import Home from './pages/Home';
-import Space from './pages/Space';
 import MyMeetings from './pages/MyMeetings';
 
 function App() {
