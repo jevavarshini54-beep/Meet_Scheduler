9e1b9407ce0c33fb631cde3967820f5465c12b50
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-23 17:53:02 +0530

    Created 2 fns called fetchMeetings and fetchSpaces to find meet/space the user is a part of

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,46 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import axios from 'axios';
+import './Home.css';
+navigate = useNavigate();
 
-function Home() {
+function Home({currentUser, setCurrentUser}) {
+
+	const [meetings, setMeetings] = useState([]);
+	const [spaces, setSpaces] = useState([]);
+
+	const fetchMeetings = async () => {
+		try{
+			const res = await axios.get(`/api/meetings/user/${currentUser._id}`);
+			setMeetings(res.data.meetings);
+		}
+
+		catch(err){
+			console.log("Error in fetching meets : ",err);
+		}
+	};
+
+	// meeting spaces users are a part of
+	const fetchSpaces = async () => {
+		try{
+			const res = await axios.get(`api/spaces/user/${currentUser._id}`);
+			setSpaces(res.data.spaces);
+		}
+
+		catch(err){
+			console.log("Error in finding spaces : ", err);
+		}
+	};
+
+	const getDaysInMonth = (date) => {
+		return Date(date.getFullYear(), date.getFullMonth()+1, 0).getDay();
+	};
+
+	const getFirstDayOfMonth = (date) => {
+		return Date(date.getFullYear(), date.getFullMonth(), 1).getDate();
+	}
+
   return(
     <h1>Home</h1>
   );
