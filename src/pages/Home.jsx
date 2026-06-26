f0dc50dcbf6358054f25a7b611e1be8e9e4020fa
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-26 21:54:10 +0530

    Added responsiveness to Login page and animations to Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { motion, AnimatePresence, spring, color } from 'framer-motion';
+import { motion, AnimatePresence, spring, color, animate, easeIn, easeOut } from 'framer-motion';
 import { IconTrash, IconArrowBack } from "@tabler/icons-react";
 import axios from 'axios';
 import './Home.css';
@@ -141,19 +141,25 @@ function Home({currentUser, setCurrentUser}) {
 	const firstDay = getFirstDayOfMonth(currentMonth);
 	const daysInMonth = getDaysInMonth(currentMonth);
 
+	const navBar = {initial: {y: -20, opacity: 0}, animate: {y: 0, opacity: 1, transition: {duration: 1.6, ease: easeIn}}, exit: {y: -20, opacity: 0}};
+	const leftCont = {initial: {x: -60, opacity: 0}, animate: {x: 0, opacity: 1, transition: {duration: 2, ease: easeIn}}, exit: {x: -60, opacity: 0}};
+	const rightCont = {initial: {x: 60, opacity: 0}, animate: {x: 0, opacity: 1, transition: {duration: 2, ease: easeIn}}, exit: {x: 60, opacity: 0}};
+	const leftMeet = {initial: {x: 18, opacity: 0},animate: {x: 0, opacity: 1, transition: {duration: 0.7, ease: easeOut}}};
+	const rightSpace = {initial: {x: -18, opacity: 0},animate: {x: 0, opacity: 1, transition: {duration: 0.7, ease: easeOut}}};
+
   return(
 		<div>
-			<div>
+			<motion.div variants={navBar} initial="initial" animate="animate" exit="exit">
 				<div className='navbar'>
 					<h1 className='title'>Meeting Scheduler</h1>
 					<motion.button className='logoutBtn' onClick={handleLogout} whileHover={{y: 3, x: -3, opacity: 0.9}} whileTap={{y: 0, x: -3, opacity: 0.83}}>Logout</motion.button>
 				</div>
 				<h1 className='user_name'>Hi, {currentUser?.username}</h1>
-			</div>
+			</motion.div>
 			
 			<div className='left_right'>
 				<AnimatePresence>
-					<div className='left_cont'>
+					<motion.div className='left_cont' variants={leftCont} initial="initial" animate="animate" exit="exit">
 						{showCalendar && (
 						<div>
 							<div className='buttons'>
@@ -190,25 +196,25 @@ function Home({currentUser, setCurrentUser}) {
 								<button onClick={handleBack} className='back_btn'><IconArrowBack size={20} /></button>
 
 								{getMeetingsForDate(selectedDate).length === 0 ? (
-									<div className='no_meet'>No meetings for this day</div>
+									<motion.div className='no_meet' variants={leftMeet} initial="initial" animate="animate">No meetings for this day</motion.div>
 								) : (
 									getMeetingsForDate(selectedDate).map(m => (
-										<div className='behind_cal' key={m._id}>
+										<motion.div className='behind_cal' key={m._id} variants={leftMeet} initial="initial" animate="animate">
 											<p>Title : {m.title}</p>
 											<p>Details : {new Date(m.startTime).toLocaleDateString()} •{' '}
 											{new Date(m.startTime).toLocaleTimeString([], {hour: '2-digit', minute: '2-digit'})} •{' '}
 											{m.duration} mins</p>
 											{m.description && <p>Description : {m.description}</p>}
 											<p>Organised By : {m.createdBy?.username}</p>
-										</div>
+										</motion.div>
 									))
 								)}
 							</div>
 						)}
-					</div>
+					</motion.div>
 				</AnimatePresence>
 
-				<div className='right_cont'>
+				<motion.div className='right_cont' variants={rightCont} initial="initial" animate="animate" exit="exit">
 
 					<div className='right_cont_btns'>
 						<motion.button whileHover={{scale: 1.09, opacity: 0.7, y: 3, transition: {type: "spring", stiffness: 300, damping: 20, duration: 0.5}}}
@@ -224,11 +230,12 @@ function Home({currentUser, setCurrentUser}) {
 
 					<div className='heading'>Your Spaces :</div>
 					{spaces.length === 0 ? (
-						<div className='no_space'>No spaces yet!</div>
+						<div className='no_space' variants={rightSpace} initial="initial" animate="animate">No spaces yet!</div>
 					) : (
 						spaces.map((space, i) => (
 							<motion.div onClick={() => navigate(`/space/${space._id}`)} className='space' key={space._id}
-								whileHover={{scale: 1.02, transition: {type: "spring", stiffness: 300, damping: 10}}}>
+								whileHover={{scale: 1.02, transition: {type: "spring", stiffness: 300, damping: 10}}}
+								 variants={rightSpace} initial="initial" animate="animate">
 								<div className='space_box'>• <span>Space Name : </span>{space.name} •</div>
 								<div className='space_box'>• <span>Space Code : </span>{space.code} •</div>
 							</motion.div>
@@ -262,7 +269,7 @@ function Home({currentUser, setCurrentUser}) {
 							</div>
 						</div>
 					}
-				</div>
+				</motion.div>
 			</div>
 		</div>
   );
