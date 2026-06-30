ca73dd34d1b8866c0e0bb40a089a5d663daa9103
Jeva Varshini S <jevavarshini54@gamil.com>
2026-06-30 22:23:44 +0530

    Setup authentication controller

---
+++ Controllers/AuthControllers.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const validator = require('validator');
+
+const signup = async(req, res) => {
+
+};
+
+const login = async(req, res) => {
+};
+
+const logout = async(req, res) => {
+};
+
+module.exports = {signup, login, logout};
