61f9e6686451f326813526f74b5e839805245dbc
Jeva Varshini S <jevavarshini54@gamil.com>
2026-07-01 16:45:58 +0530

    Implemented user login - using email and password

--- Controllers/AuthControllers.js
+++ Controllers/AuthControllers.js
@@ -1,11 +1,25 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const { BaseCollection } = require('mongoose');
 const validator = require('validator');
 
 const signup = async(req, res) => {
 	try {
 		const {username, email, password} = req.body;
+
+		if (!username || !password || !email) {
+			return res.status(400).json({message: "All fields are required"});
+		}
+
+		if (!validator.isEmail(email)) {
+			return res.status(400).json({message: "Please enter a valid email"});
+		}
+
+		const existingUser = await User.findOne({email});
+		if (existingUser) {
+			return res.status(400).json({message: "Email already registered"});
+		}
 
 		if (password.length < 8){
 			return res.status(400).json({message: "Password must be atleast 8 characters"});
@@ -27,19 +41,6 @@ const signup = async(req, res) => {
 			return res.status(400).json({message: "Password must contain a special character"});
 		}
 
-		if (!username || !password || !email) {
-			return res.status(400).json({message: "All fields are required"});
-		}
-
-		if (!validator.isEmail(email)) {
-			return res.status(400).json({message: "Please enter a valid email"});
-		}
-
-		const existingUser = await User.findOne({email});
-		if (existingUser) {
-			return res.status(400).json({message: "Email already registered"});
-		}
-
 		const salt = await bcrypt.genSalt();
 		const hashedPassword = await bcrypt.hash(password,salt);
 		const user = await User.create({
@@ -50,9 +51,10 @@ const signup = async(req, res) => {
 		return res.status(201).json({message: "User created successfully",
 			user: {
 				_id: user._id,
-				name: user.username,
+				username: user.username,
 				email: user.email
-			}});
+			}
+		});
 	}
 
 	catch(err) {
@@ -61,6 +63,43 @@ const signup = async(req, res) => {
 };
 
 const login = async(req, res) => {
+	try {
+		const {email, password} = req.body;
+
+		if (!email || !password) {
+			return res.status.json({message: "All fields are required"});
+		}
+
+		if (!validator.isEmail(email)) {
+			return res.status(400).json({message: "Please enter a valid email"});
+		}
+
+		const user = await User.findOne({email});
+		if (!user) {
+			return res.status(400).json({message: "Email id doesn't exist"});
+		}
+
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			res.status(400).json({message: "Invalid credentials"})
+		};
+
+		const token = jwt.sign(
+			{id: user._id},
+			process.env.JWT_SECRET,
+			{expiresIn: "30m"}
+		);
+
+		if (!token) {
+			return res.status(400).json({message: "Token not created"});
+		}
+
+		res.status(200).json({message: "Login successful"});
+	}
+
+	catch {
+		return res.status(400).json({message: "Unsuccessful login"});
+	}
 };
 
 const logout = async(req, res) => {
---
+++ ProtectedRoutes/AuthMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require('jsonwebtoken');
+
+const authMiddleware = (req, res, next) => {
+  const token = req.headers.authorization?.split(" ")[1];
+
+	if (!token) {
+		return res.status(400).json({message: "Token not found"});
+	}
+
+	try {
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = decoded;
+		next();
+	}
+
+	catch {
+		return res.status(400).json({message: "Invalid token"});
+	}
+}
